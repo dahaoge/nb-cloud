467149b867cf2622047a06578b26f45209c4a103
dahaoge <179314039@qq.com>
2019-12-05 15:55:17 +0800

    gateway

--- nb-gateway/src/main/java/cn/hao/nb/cloud/gateway/GatewayApplication.java
+++ nb-gateway/src/main/java/cn/hao/nb/cloud/gateway/GatewayApplication.java
@@ -1,7 +1,6 @@
 package cn.hao.nb.cloud.gateway;
 
-//import lombok.extern.slf4j.Slf4j;
-
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -11,7 +10,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  * @Date: 2019-11-30 09:49
  * @Description:
  */
-//@Slf4j
+@Slf4j
 @SpringBootApplication
 @EnableDiscoveryClient
 public class GatewayApplication {
@@ -19,7 +18,7 @@ public class GatewayApplication {
     public static void main(String[] args) {
 //        try {
         SpringApplication.run(GatewayApplication.class, args);
-//        log.info("\033[1;93;32m【Server startup done.】\033[m");
+        log.info("\033[1;93;32m【Server startup done.】\033[m");
 //        }catch (Exception e){
 //            log.error("服务xxx-support启动报错", e);
 //        }
