ba3ce6a5ef298e1d2d5e84194db8447d2b0c9c5a
dahaoge <179314039@qq.com>
2019-12-05 14:11:17 +0800

    common init

--- nacos-demo/src/main/java/cn/hao/nb/demo/DemoApplication.java
+++ nacos-demo/src/main/java/cn/hao/nb/demo/DemoApplication.java
@@ -4,9 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 /**
  * @Auther: hao
